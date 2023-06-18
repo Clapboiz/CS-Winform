93f95db479d626d18a5a7aa576dd97014e491e56
Clapboiz <phamconglap03@gmail.com>
2023-06-18 16:12:57 +0700

    Lab02

---
+++ Lab02/Bai05/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Bai05;
+
+partial class Form1
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select Directory";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(202, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "path";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(255, 24);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(391, 34);
+            this.textBox1.TabIndex = 2;
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(26, 82);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(620, 356);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private Button button1;
+    private Label label1;
+    private TextBox textBox1;
+    private ListView listView1;
+}
---
+++ Lab02/Bai05/Form1.cs
@@ -0,0 +1,52 @@
+﻿namespace Bai05;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+        //Khởi tạo các cột trong listview.
+        listView1.View = View.Details;
+        listView1.Columns.Add("Tên");
+        listView1.Columns.Add("Kích thước");
+        listView1.Columns.Add("Đuôi mở rộng");
+        listView1.Columns.Add("Ngày khởi tạo");
+    }
+
+    private void btnRead_Click(object sender, EventArgs e)
+    {
+        //FolderBrowserDialog là một hộp thoại cho phép người dùng chọn một thư mục.
+        FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+        //Hiển thị hộp thoại và kiểm tra xem người dùng đã chọn một thư mục và nhấn OK hay chưa.
+        if (fbd.ShowDialog() == DialogResult.OK)
+        {
+            textBox1.Text = fbd.SelectedPath;
+
+            //Tạo một đối tượng DirectoryInfo với đường dẫn được chọn bởi người dùng.
+            DirectoryInfo di = new DirectoryInfo(fbd.SelectedPath);
+            //Lấy danh sách các tệp tin trong thư mục được chọn bởi người dùng và lưu trữ chúng trong một mảng các đối tượng FileInfo.
+            FileInfo[] fiArr = di.GetFiles();
+
+            //Lặp qua từng đối tượng FileInfo trong mảng fiArr.
+            foreach (FileInfo file in fiArr)
+            {
+                ListViewItem item = new ListViewItem();
+                //Đặt giá trị của thuộc tính Text của ListViewItem thành tên của tệp tin.
+                item.Text = file.Name;
+
+                //Thêm một mục con mới vào ListViewItem, đại diện cho đuôi mở rộng của tệp tin,
+                //và đặt giá trị của nó thành đuôi mở rộng của tệp tin.
+                item.SubItems.Add(new ListViewItem.ListViewSubItem() { Text = file.Length.ToString() });
+                item.SubItems.Add(new ListViewItem.ListViewSubItem() { Text = file.Extension });
+                item.SubItems.Add(new ListViewItem.ListViewSubItem() { Text = file.CreationTime.ToString() });
+                listView1.Items.Add(item);
+            }
+        }
+    }
+
+    private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+}
